fda90c1f3b4e8cdb887ae41a3ef8ebdc5befd1eb
mariam <farouuukmarmar@gmail.com>
2024-08-15 22:12:41 +0300

    add welcomefragmet

--- app/src/main/java/com/example/alakefak/ui/authflow/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/alakefak/ui/authflow/welcome/WelcomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.alakefak.ui.authflow.welcome
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import com.example.alakefak.R
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.alakefak.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -15,10 +14,12 @@ class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_welcome, container, false
-        )
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
 
+        binding.button.setOnClickListener {
+
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToRegisterFragment())
+        }
 
         return binding.root
     }
