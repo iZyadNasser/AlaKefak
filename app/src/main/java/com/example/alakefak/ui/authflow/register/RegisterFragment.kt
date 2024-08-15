6785788ca46e3c97c2acdb6c154f4223f40b5ddb
Zyad <zyadhammad531@gmail.com>
2024-08-15 14:52:33 -0700

    added animations to transitions

--- app/src/main/java/com/example/alakefak/ui/authflow/register/RegisterFragment.kt
+++ app/src/main/java/com/example/alakefak/ui/authflow/register/RegisterFragment.kt
@@ -1,18 +1,61 @@
 package com.example.alakefak.ui.authflow.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.alakefak.R
+import com.example.alakefak.databinding.FragmentRegisterBinding
+import com.example.alakefak.ui.authflow.splash.SplashFragmentDirections
 
 class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+
+        setOnClickListeners()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return binding.root
+    }
+
+    private fun setOnClickListeners() {
+        binding.loginTextView.setOnClickListener {
+            setNavigationWithAnimation()
+        }
+    }
+
+    private fun setNavigationWithAnimation() {
+        val swipeOut = AnimationUtils.loadAnimation(requireContext(), R.anim.swipe_out_short)
+
+        swipeOut.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.root.visibility = View.GONE
+
+                findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+        })
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.root.startAnimation(swipeOut)
+        }, 5)
     }
 }
--- app/src/main/java/com/example/alakefak/ui/authflow/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/example/alakefak/ui/authflow/welcome/WelcomeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.alakefak.ui.authflow.welcome
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.alakefak.R
 import com.example.alakefak.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -23,7 +28,30 @@ class WelcomeFragment : Fragment() {
 
     private fun setOnClickListeners() {
         binding.button.setOnClickListener {
-            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToRegisterFragment())
+            setNavigationWithAnimation()
         }
+    }
+    private fun setNavigationWithAnimation() {
+        val swipeOut = AnimationUtils.loadAnimation(requireContext(), R.anim.swipe_out_short)
+
+        swipeOut.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                binding.root.visibility = View.GONE
+
+                findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToRegisterFragment())
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+        })
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding.root.startAnimation(swipeOut)
+        }, 5)
     }
 }
