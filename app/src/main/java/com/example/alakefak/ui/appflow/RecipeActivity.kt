3a84cc1ea9500e253242f319598a711552e13fb0
Zyad <zyadhammad531@gmail.com>
2024-08-16 10:58:19 -0700

    Added navigation logic

--- app/src/main/java/com/example/alakefak/ui/appflow/RecipeActivity.kt
+++ app/src/main/java/com/example/alakefak/ui/appflow/RecipeActivity.kt
@@ -1,13 +1,47 @@
 package com.example.alakefak.ui.appflow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.alakefak.R
+import com.example.alakefak.databinding.ActivityRecipeBinding
+import com.example.alakefak.ui.appflow.favourites.FavouritesFragment
+import com.example.alakefak.ui.appflow.home.HomeFragment
+import com.example.alakefak.ui.appflow.settings.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class RecipeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecipeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
+        binding = ActivityRecipeBinding.inflate(layoutInflater)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        navView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.fav -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, FavouritesFragment())
+                        .commit()
+                    true
+                }
+                R.id.settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
 }
