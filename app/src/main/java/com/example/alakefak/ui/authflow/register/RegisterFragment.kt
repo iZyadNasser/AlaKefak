58f74534d77fed552b99dcffb54a412a4a233604
Zyad <zyadhammad531@gmail.com>
2024-08-18 00:22:57 -0700

    refactored the code

--- app/src/main/java/com/example/alakefak/ui/authflow/register/RegisterFragment.kt
+++ app/src/main/java/com/example/alakefak/ui/authflow/register/RegisterFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.alakefak.R
@@ -41,7 +40,18 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.errorState.observe(viewLifecycleOwner, Observer {
+        observeFormState()
+        usernameFocusListener()
+        emailFocusListener()
+        passwordFocusListener()
+        confirmPasswordFocusListener()
+        setupTextWatchers()
+        handleOnClicks()
+        singInForm()
+    }
+
+    private fun observeFormState() {
+        viewModel.errorState.observe(viewLifecycleOwner) {
             when (viewModel.errorState.value) {
                 ErrorStates.EMAIL -> {
                     Toast.makeText(
@@ -76,15 +86,7 @@ class RegisterFragment : Fragment() {
 
                 else -> {}
             }
-        })
-
-        usernameFocusListener()
-        emailFocusListener()
-        passwordFocusListener()
-        confirmPasswordFocusListener()
-        setupTextWatchers()
-        handleOnClicks()
-        singInForm()
+        }
     }
 
     private fun usernameFocusListener() {
