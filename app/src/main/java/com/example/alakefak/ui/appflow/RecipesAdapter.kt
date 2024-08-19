aeab8f569e1c971c617841509c2bdacce68e82ba
Malak <malak.raef20@gmail.com>
2024-08-19 14:47:28 +0300

    making some changes to the favorites adapter

--- app/src/main/java/com/example/alakefak/ui/appflow/RecipesAdapter.kt
+++ app/src/main/java/com/example/alakefak/ui/appflow/RecipesAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.alakefak.R
 
 class RecipesAdapter(val myList: ArrayList<RecipeResponse>) :
