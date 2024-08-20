036eac365833d802fdc89a5a3697b1e77f40680c
Malak <malak.raef20@gmail.com>
2024-08-20 18:29:46 +0300

    modify favorites branch

---
+++ app/src/main/java/com/example/alakefak/data/source/local/model/FavoritesInfo.kt
@@ -0,0 +1,14 @@
+package com.example.alakefak.data.source.local.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favoritesInfo")
+data class FavoritesInfo(
+    @PrimaryKey var id: String,
+    @ColumnInfo(name = "strMeal") val recipeName: String,
+    @ColumnInfo(name = "strCategory") val recipeCategory: String,
+    @ColumnInfo(name = "strMealThumb") val recipeImg: String,
+    @ColumnInfo(name = "strArea") val area : String
+)
