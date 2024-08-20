9245aee3fba10cb1e1a7491637be36d01bc15acf
Zyad <zyadhammad531@gmail.com>
2024-08-20 15:43:07 +0300

    Edited the search view layout

--- app/src/main/java/com/example/alakefak/ui/appflow/search/SearchFragmentViewModel.kt
+++ app/src/main/java/com/example/alakefak/ui/appflow/search/SearchFragmentViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.alakefak.ui.appflow.search
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
