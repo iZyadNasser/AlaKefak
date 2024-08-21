999a2b779c8ec586ebc44df2bfc5d393cbc22ef5
Malak <malak.raef20@gmail.com>
2024-08-21 23:41:03 +0300

    Adding CategoriesAdapter.kt

--- app/src/main/java/com/example/alakefak/ui/appflow/home/CategoriesAdapter.kt
+++ app/src/main/java/com/example/alakefak/ui/appflow/home/CategoriesAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.alakefak.ui.appflow.home
 
-class CategoriesAdapter {
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alakefak.R
+
+class CategoriesAdapter(val items: List<String>) : RecyclerView.Adapter<CategoriesAdapter.MyViewHolder>() {
+    class MyViewHolder(val col: View) : RecyclerView.ViewHolder(col) {
+        var categoryBtn = col.findViewById<Button>(R.id.categoryBtn)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater =
+            parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val col = inflater.inflate(R.layout.my_col_item, parent, false)
+        return MyViewHolder(col)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = items.getOrNull(position)
+        if (item != null) {
+            holder.categoryBtn.text = item
+        }
+    }
+
+    override fun getItemCount(): Int = items.size
+
 }
