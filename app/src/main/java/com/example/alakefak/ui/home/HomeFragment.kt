08bbe2ebd23d62cc0b5ac8b29e4f560fc7ee3b67
mariam <farouuukmarmar@gmail.com>
2024-08-22 00:45:51 +0300

    adding sign out dialog modify

--- app/src/main/java/com/example/alakefak/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/alakefak/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.alakefak.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -35,13 +36,19 @@ class HomeFragment : Fragment() {
             setPositiveButton("Sign out") { dialog, _ ->
                 Toast.makeText(context, "Signed out successfully", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
-                activity?.finish()
+                navigateToRegisterFragment()
             }
             setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }
             show()
         }
+    }
+
+    private fun navigateToRegisterFragment() {
+
+        val action = HomeFragmentDirections.actionHomeFragmentToRegisterFragment()
+        findNavController().navigate(action)
     }
 
     override fun onDestroyView() {
