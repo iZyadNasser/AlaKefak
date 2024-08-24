1080f4ab95cbdf52619a9e68505dae851c8c775a
mariam <farouuukmarmar@gmail.com>
2024-08-24 19:19:52 +0300

    editing details fragment youtube

--- app/src/main/java/com/example/alakefak/ui/appflow/details/DetailsFragment.kt
+++ app/src/main/java/com/example/alakefak/ui/appflow/details/DetailsFragment.kt
@@ -1,61 +1,70 @@
 package com.example.alakefak.ui.appflow.details
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.alakefak.R
 import com.example.alakefak.databinding.FragmentDetailsBinding
 import com.example.alakefak.data.source.remote.model.Meal
-import com.example.alakefak.ui.appflow.home.HomeFragment
 
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     private val viewModel: DetailsViewModel by viewModels()
     private lateinit var binding: FragmentDetailsBinding
     private var meal: Meal = Meal()
-    private  var mealId:String=""
-
+    private var mealId: String = ""
+    private var isPlayerViewVisible = false
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailsBinding.bind(view)
-//        arguments?.let { bundle ->
-//            mealId = bundle.getString("MEAL_ID").toString()
-//
-//        }
 
-        mealId = HomeFragment.clickedMeal?.getString("MEAL_ID").toString()
+        binding.playerView.settings.javaScriptEnabled = true
+
+
+        mealId = arguments?.getString("MEAL_ID") ?: ""
 
         viewModel.getMeal(mealId)
-        viewModel.notifyMealFetched.observe(viewLifecycleOwner) {
-
-            if (it != null) {
-                meal = it
-                binding.ingredients.apply {
+        viewModel.notifyMealFetched.observe(viewLifecycleOwner) { fetchedMeal ->
+            fetchedMeal?.let { meal ->
+                this.meal = meal
+                binding.content.apply {
                     layoutManager = LinearLayoutManager(context)
                     adapter = IngredientAdapter(viewModel.covertIngredients(meal))
                 }
 
-                binding.FooodName.text = meal.strMeal
-                binding.category.text = meal.strCategory
-                binding.area.text = meal.strArea
-                binding.instructiontext.text = meal.strInstructions
-
+                binding.categoryInfo.text = meal.strCategory
+                binding.areaInfo.text = meal.strArea
+                binding.instructions.text = meal.strInstructions
+                binding.foodname.text = meal.strMeal
 
                 Glide.with(this@DetailsFragment)
                     .load(meal.strMealThumb)
-                    .into(binding.imageView)
+                    .into(binding.imgMealDetail)
             }
+            binding.imgYoutube.setOnClickListener {
+
+                isPlayerViewVisible = !isPlayerViewVisible
+
+                if (isPlayerViewVisible) {
+
+                    binding.playerView.visibility = View.VISIBLE
+                    val videoUrl = meal.strYoutube?.replace("watch?v=", "embed/")
+                    videoUrl?.let { url ->
+                        binding.playerView.loadUrl(url)
+                    }
+                } else {
+
+                    binding.playerView.visibility = View.GONE
+                }
+            }
+
         }
 
 
-
-
-
     }
-
 }
-
